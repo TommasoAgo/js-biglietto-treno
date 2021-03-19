37df75c3f5db059dcc37ac0ff24fc90cf222ac52
Tommaso Agostini <tommasoagostini90@gmail.com>
2021-03-19 16:03:21 +0100

    Procedo con il layout

--- js/script.js
+++ js/script.js
@@ -5,12 +5,12 @@
 // Chiedo all'utente il numero di chilometri che vuole percorrere
 var kmPercorsi = prompt ('Quanti km devi percorrere?');
 kmPercorsi = parseInt (kmPercorsi);
-console.log(kmPercorsi);
+
 
 // Chiedo all'utente la sua età
 var laTuaEta = prompt ('Inserisci la tua età');
 laTuaEta = parseInt (laTuaEta);
-console.log(laTuaEta);
+
 
 // Prezzo biglietto (0.21€ al km)
 var prezzoBiglietto = kmPercorsi * 0.21;
@@ -20,10 +20,10 @@ if ( laTuaEta <= 18 ) {
     prezzoBiglietto = prezzoBiglietto - (prezzoBiglietto * 0.2);
 // Se l'età è superiore ai 65 anni, allora si applica uno sconto del 40%
 } else if ( laTuaEta > 65) {
-    prezzoBiglietto = prezzoBiglietto - (prezzoBiglietto * 0.4)
+    prezzoBiglietto = prezzoBiglietto - (prezzoBiglietto * 0.4);
 } else {
     prezzoBiglietto = prezzoBiglietto
-}
-console.log(prezzoBiglietto)
+};
 
 // Stampare il prezzo finale con massimo due numeri decimali
+document.getElementById('prezzo-biglietto').innerHTML = prezzoBiglietto.toFixed(2);
